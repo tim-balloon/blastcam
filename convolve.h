e42a8c6c13e2e3fd8f1e4dada95e685a07c4e3fb
starcam <starcam@SC1.foo>
2025-08-12 12:01:44 -0700

    Remove unused convolution functions

--- convolve.h
+++ convolve.h
@@ -13,38 +13,12 @@ int imageStats(
     float* pAverage,
     float* pMax,
     uint32_t* pIdxMax);
-void readROI(
-    float* pImageBuffer,
-    uint8_t* pMask,
-    uint32_t ROIcenter,
-    uint8_t ROIsideLength,
-    uint16_t imageWidth,
-    uint32_t imageNumPix,
-    float* pROIbuffer,
-    uint32_t* pROInumPixRead,
-    uint8_t* pROIsideLengthRead);
 void doConvolution3x3(
     float* pImageBuffer,
     uint8_t* pMask,
     uint16_t imageWidth,
     uint32_t imageNumPix,
     float* pKernel,
-    float* pImageResult);
-void doConvolutionNxN(
-    float* pImageBuffer,
-    uint8_t* pMask,
-    uint16_t imageWidth,
-    uint32_t imageNumPix,
-    float* pKernel,
-    uint8_t kernelSideLength,
-    float* pImageResult);
-void doBoxcarNxN(
-    float* pImageBuffer,
-    uint8_t* pMask,
-    uint16_t imageWidth,
-    uint32_t imageNumPix,
-    uint8_t radius,
-    float* pIntermediate,
     float* pImageResult);
 
 #endif
