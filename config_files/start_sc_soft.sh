626eaa6e6029013dda44702321cf3ee8b981cd2d
starcam <starcam@SC1.foo>
2025-08-29 22:36:27 -0700

    Fix service startup bug, tmp script config for ecm branch executable

--- config_files/start_sc_soft.sh
+++ config_files/start_sc_soft.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo /home/github/blastcam/build/commands.sh -c 1 -s /dev/ttyLens1port8001 -p 8001
+# /home/starcam/github/blastcam/build/commands.sh -c 1 -s /dev/ttyLens1port8001 -p 8001;
+/home/starcam/evanmayer/blastcam/build/commands.sh -c 1 -s /dev/ttyLens1port8001 -p 8001
